405188ff7268b9f8c3a6a5fc07a8c9053ff55861
Shahrukh <117969497+Shahrukh492@users.noreply.github.com>
2023-10-17 18:54:21 +0530

    Python_GmailAPI_Read.py

---
+++ Python_GmailAPI_Read.py
@@ -0,0 +1,48 @@
+import os
+import base64
+import json
+import logging
+import time
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+CREDS_FILE = 'client_secret_apps.json'
+
+def create_gmail_service():
+    """Creates a Gmail API service using credentials from credentials.json."""
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+def list_labels():
+    """Lists the labels in the authenticated Gmail account."""
+    service = create_gmail_service()
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
+
+if __name__ == '__main__':
+    list_labels()
